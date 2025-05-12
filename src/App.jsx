b541ed0d59b8dbc3b993b8420a519939e5d920b6
Ahsan Burki <ahsanburki2126@gmail.com>
2025-05-12 17:53:06 +0500

    pages case issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 
 import About from "./pages/About";
 import Products from "./pages/Products";
